2ac7c480e42902cf8b7979b0d5fdb1613b5658d8
Duncan Fermor <duncan@elitemail.org>
2015-05-16 15:10:12 +0100

    stopped calling publisher and subscriber the same thing

--- features/step_definitions/reporter_steps.rb
+++ features/step_definitions/reporter_steps.rb
@@ -7,7 +7,8 @@ require 'json'
 
 Before('@mosquitto') do |scenario|
 	stub_request(:any, 'foo.com')
-	@pid = spawn 'echo "log_dest none" | mosquitto -c /dev/stdin'
+	#@pid = spawn 'echo "log_dest none" | mosquitto -c /dev/stdin'
+	@pid = spawn 'mosquitto'
 end
 
 After('@mosquitto') do |scenario|
@@ -15,12 +16,12 @@ After('@mosquitto') do |scenario|
 end
 
 Given /the reporter is pointed at (\S+)/ do |target_uri|
-	@done = false
-	reader = Mosquitto::Client.new("blocking")
+	@done = []
+	reader = Mosquitto::Client.new("reporter")
 	reader.loop_start
 
 	reader.on_message do |m|
-		#puts "Reader: relaying '#{ m.to_s }'"
+		puts "Reader: relaying '#{ m.to_s }'"
 
 		uri = URI.parse("http://" + target_uri)
 		http = Net::HTTP.new(uri.host, uri.port)
@@ -29,35 +30,51 @@ Given /the reporter is pointed at (\S+)/ do |target_uri|
 		request["Content-Type"] = "application/json"
 		response = http.request(request)
 
-		@done = true
+		@done << m.to_s
 	end
+
 
 	reader.on_connect do |rc|
-		#puts "Reader: Connected with return code #{rc}"
-		reader.subscribe(3, "topic", Mosquitto::AT_MOST_ONCE)
+		puts "Reader: Connected with return code #{rc}"
 	end
 
-	reader.connect("localhost", 1883, 10)
+	reader.on_subscribe do |mid, qos|
+		puts "subscribed with mid #{mid} and qos #{qos}"
+	end
+
+	reader.connect("localhost", 1883, 1000)
+
+	@reader_ready = false
+	reader.on_connect do |rc|
+		@reader_ready = true
+	end
+
+	Timeout::timeout(3) {
+		while not @reader_ready do
+		end
+		reader.subscribe(nil, "dns_lookup", Mosquitto::EXACTLY_ONCE)
+	}
 end
 
 When 'there is an incoming result' do
-	publisher = Mosquitto::Client.new("blocking")
+	publisher = Mosquitto::Client.new("publisher")
 	publisher.loop_start
 
 	publisher.on_publish do |mid|
-		#puts "Dummy Publisher: Published #{mid}"
-		publisher.disconnect
+		puts "Dummy Publisher: Published #{mid}"
 	end
 
-	publisher.connect("localhost", 1883, 10)
+	publisher.connect("localhost", 1883, 1000)
 
 	publisher.on_connect do |rc|
-		#puts "Dummy Publisher: Connected with return code #{rc}"
-		publisher.publish(nil, "topic", "test message", Mosquitto::AT_MOST_ONCE, true)
+		puts "Dummy Publisher: Connected with return code #{rc}"
+		publisher.publish(nil, "dns_lookup", "test message", Mosquitto::EXACTLY_ONCE, false)
+		publisher.publish(nil, "dns_lookup", "test message 2", Mosquitto::EXACTLY_ONCE, false)
+		publisher.publish(nil, "dns_lookup", "test message 3", Mosquitto::EXACTLY_ONCE, false)
 	end
 
-	Timeout::timeout(5) {
-		while not @done do
+	Timeout::timeout(7) {
+		while not @done.sort == ["test message", "test message 2", "test message 3"] do
 		end
 	}
 end
@@ -71,5 +88,7 @@ Then /a report is posted to (\S+) as json/ do |target_uri|
 			'1.1.1.3' => false
 		}
 	}
-	expect(a_request(:post, uri).with { |req| req.body.values.include? "test message" }).to have_been_made
+	expect(a_request(:post, uri).with { |req| req.body.values.include? "test message" }).to have_been_made.once
+	expect(a_request(:post, uri).with { |req| req.body.values.include? "test message 2" }).to have_been_made.once
+	expect(a_request(:post, uri).with { |req| req.body.values.include? "test message 3" }).to have_been_made.once
 end
