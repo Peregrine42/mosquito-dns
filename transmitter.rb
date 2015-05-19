f8b54a2184b141f78ffc5da0f5f5940fecc827ca
Duncan Fermor <duncan@elitemail.org>
2015-05-19 09:06:34 +0100

    refactored transmitter

--- transmitter.rb
+++ transmitter.rb
@@ -1,4 +1,3 @@
-require 'json'
 require 'uri'
 require 'net/http'
 require './mosquitto_client'
@@ -10,15 +9,21 @@ class Transmitter
 	end
 
 	def post report
-		post_over_http report, @host
+		post_over_http report
 	end
 
-	def post_over_http some_json, target_uri
-		uri = URI.parse("http://" + target_uri)
+	def post_over_http some_json
+		uri = URI.parse("http://" + @host)
+		request = post_request uri, some_json
 		http = Net::HTTP.new(uri.host, uri.port)
+		http.request(request)
+	end
+
+	private
+	def post_request uri, content
 		request = Net::HTTP::Post.new(uri.request_uri)
-		request.body = some_json
+		request.body = content
 		request["Content-Type"] = "application/json"
-		response = http.request(request)
+		request
 	end
 end
